7eb85c95aa8307a6d6c528567d10de6b96ded068
Alan Ke Sun <alan.k.sun@gmail.com>
2017-11-29 12:58:49 +0100

    intitial commit

---
+++ antlr-calculator/src/main/java/net/codenest/antlr/calculator/Calculator.java
@@ -0,0 +1,18 @@
+package net.codenest.antlr.calculator;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+public class Calculator {
+	public static void main(String[] args) throws Exception {
+		// ANTLRInputStream input = new ANTLRInputStream(System.in);
+		// CalculatorLexer lexer = new CalculatorLexer(input);
+		// CommonTokenStream tokens = new CommonTokenStream(lexer);
+		// CalculatorParser parser = new CalculatorParser(tokens);
+		// ParseTree tree = parser.input();
+		//
+		// CalculatorVisitor visitor = new CalculatorVisitor();
+		// Double result = visitor.visit(tree);
+		// System.out.println("Result: " + result);
+	}
+}
---
+++ antlr-calculator/src/main/java/net/codenest/antlr/calculator/CalculatorVisitorImpl.java
@@ -0,0 +1,5 @@
+package net.codenest.antlr.calculator;
+
+public class CalculatorVisitorImpl extends CalculatorBaseVisitor<Double> {
+
+}
