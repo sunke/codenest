2b34ecec3742bafdcdc77ec615321579286f96bf
Alan <alan.k.sun@gmail.com>
2017-11-29 22:17:18 +0100

    Add visitor implementation

--- antlr-calculator/src/main/java/net/codenest/antlr/calculator/Calculator.java
+++ antlr-calculator/src/main/java/net/codenest/antlr/calculator/Calculator.java
@@ -5,14 +5,14 @@ import org.antlr.v4.runtime.tree.*;
 
 public class Calculator {
 	public static void main(String[] args) throws Exception {
-		// ANTLRInputStream input = new ANTLRInputStream(System.in);
-		// CalculatorLexer lexer = new CalculatorLexer(input);
-		// CommonTokenStream tokens = new CommonTokenStream(lexer);
-		// CalculatorParser parser = new CalculatorParser(tokens);
-		// ParseTree tree = parser.input();
-		//
-		// CalculatorVisitor visitor = new CalculatorVisitor();
-		// Double result = visitor.visit(tree);
-		// System.out.println("Result: " + result);
+		ANTLRInputStream input = new ANTLRInputStream("1 + 2 - 6 * 3");
+		CalculatorLexer lexer = new CalculatorLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		CalculatorParser parser = new CalculatorParser(tokens);
+		ParseTree tree = parser.input();
+
+		CalculatorVisitorImpl visitor = new CalculatorVisitorImpl();
+		Double result = visitor.visit(tree);
+		System.out.println("Result: " + result);
 	}
 }
