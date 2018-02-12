a577aeac9b28a75eb93efbc68b40afc97b742c1b
Alan Ke Sun <alan.k.sun@gmail.com>
2018-02-12 22:34:20 +0100

    Add ExceptionHanleDemo

---
+++ java-async/src/main/java/net/codenest/async/CFExceptionHandleDemo.java
@@ -0,0 +1,9 @@
+package net.codenest.async;
+
+public class CFExceptionHandleDemo {
+	
+
+	public static void main(String[] args) throws Exception {
+	
+	}
+}
--- java-async/src/main/java/net/codenest/async/CFThreadExecutionDemo.java
+++ java-async/src/main/java/net/codenest/async/CFThreadExecutionDemo.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class CFThreadDemo {
+public class CFThreadExecutionDemo {
 
 	public static void main(String[] args) throws Exception {
 		CompletableFuture.runAsync(() -> {
